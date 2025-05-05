5520a8673215eb84152b439d20299942a6bfc0e1
Gautham Krishna K <gkrishnak01@Gauthams-MacBook-Air.local>
2025-05-05 20:57:15 +0530

    Changed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(true
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
-  base : "/ViteAppTest/",
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === "production" ? "/ViteAppTest/" : "/", // 👈 replace with your actual repo name
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
-});
+}));
